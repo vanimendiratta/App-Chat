447c586295d03d07bdc3cc26422abb8491359279
vanimendiratta <123664543+vanimendiratta@users.noreply.github.com>
2023-10-05 02:30:57 +0530

    Add files via upload

---
+++ src/Component/Message.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import {HStack,Avatar,Text} from "@chakra-ui/react"
+
+const Message = ({text,uri,user="other"}) => {
+  return (
+    <HStack alignSelf={user==="me"?"flex-end":"flex-start"} bg="grey" paddingY={"2"} paddingX={user==="me"?"4":"2"} borderRadius={"base"}>
+       {
+        user==="other" && <Avatar src={uri}/>
+       }
+       
+       <Text>{text}</Text> 
+
+       {
+        user==="me" && <Avatar src={uri}/>
+       }
+    </HStack>
+  )
+}
+ 
+export default Message
---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyARhvIEwnmz8eBCYtoJ__N2MBWIsVKZUzI",
+  authDomain: "chatapp-c83fd.firebaseapp.com",
+  projectId: "chatapp-c83fd",
+  storageBucket: "chatapp-c83fd.appspot.com",
+  messagingSenderId: "881594147022",
+  appId: "1:881594147022:web:b1dee2e45c0cc85dee309f"
+};
+
+export const app = initializeApp(firebaseConfig);
